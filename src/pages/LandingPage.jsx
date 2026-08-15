1d6a62b14597416535b5e2d641055779303a1a17
AHISH17052006 <ahish.essencecore@gmail.com>
2026-08-15 17:32:22 +0530

    updated landing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/auth.css";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 
 const LandingPage = () => {
   const navigate = useNavigate(); // React Router navigation hook
