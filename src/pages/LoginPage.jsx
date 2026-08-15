bf55af404b7a7e2901306f7994de85321248f35c
AHISH17052006 <ahish.essencecore@gmail.com>
2026-08-15 17:34:45 +0530

    updated signup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/auth.css";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import InputField from "../components/InputField";
 import Button from "../components/Button";
 
